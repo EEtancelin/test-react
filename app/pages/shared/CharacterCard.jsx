c1d2d45484e36931c8a0ef9224b5c435950f3f0e
16Q45T <etancelin.edouard@gmail.com>
2017-07-06 10:42:09 +0200

    Improve the design

--- app/pages/shared/CharacterCard.jsx
+++ app/pages/shared/CharacterCard.jsx
@@ -2,31 +2,11 @@ import React, { PropTypes } from 'react';
 import { browserHistory } from 'react-router';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import CharacterCard from '../shared/CharacterCard.jsx';
 import { deleteCharacterAction } from '../../reducers/character';
 
-import './home.scss';
+import '../home/home.scss';
 
-const Home = ({ characters, properties, actions }) => (
-  <div className="home">
-    <div className="button" onClick={() => browserHistory.push(`/character`)}>Add a character</div>
-    <div className="characters">
-      <div className="characters__item characters__item--head">
-        {properties.map((property, i) => (
-          <div key={i} className="characters__property">{property.label}</div>
-        ))}
-        <div className="characters__property characters__property--button">Edit</div>
-        <div className="characters__property characters__property--button">Delete</div>
-      </div>
-      {characters.map((character, i) => (
-        <div key={i} className="characters__item">
-          {properties.map((property, j) => (
-            <div key={j}className="characters__property">{character[property.key]}</div>
-          ))}
-          <div className="characters__property button" onClick={() => browserHistory.push(`/character/${i}`)}>Edit</div>
-          <div className="characters__property button button--red" onClick={() => actions.deleteCharacterAction(i)}>Delete</div>
-        </div>
-      ))}
+const CharacterCard = ({ }) => (
       <div className="character">
       <div className="character-body">
         <div className='character-name'>
@@ -56,16 +36,12 @@ const Home = ({ characters, properties, actions }) => (
           </div>
         </div>
       </div>
-    </div>
-  </div>
 );
 
 const mapStateToProps = state => ({
-  characters: state.character.characters,
-  properties: state.character.properties
 });
 
 const mapDispatchToProps = dispatch => ({
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(Home);
+export default connect(mapStateToProps, mapDispatchToProps)(CharacterCard);
