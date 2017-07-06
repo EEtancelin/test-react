926cabc3fca7bf07a4f434f80d0795c34b92a19e
16Q45T <etancelin.edouard@gmail.com>
2017-07-06 12:37:48 +0200

    Delte the last table.

--- app/pages/home/index.js
+++ app/pages/home/index.js
@@ -11,21 +11,9 @@ const Home = ({ characters, properties, actions }) => (
   <div className="home">
     <div className="button" onClick={() => browserHistory.push(`/character`)}>Add a character</div>
     <div className="characters">
-      <div className="characters__item characters__item--head">
-        {properties.map((property, i) => (
-          <div key={i} className="characters__property">{property.label}</div>
-        ))}
-        <div className="characters__property characters__property--button">Edit</div>
-        <div className="characters__property characters__property--button">Delete</div>
-      </div>
       {characters.map((character, i) => (
         <div key={i} className="characters__item">
             <CharacterCard character={character} id={i} action={actions}/>
-          {properties.map((property, j) => (
-            <div key={j}className="characters__property">{character[property.key]}</div>
-          ))}
-          <div className="characters__property button" onClick={() => browserHistory.push(`/character/${i}`)}>Edit</div>
-          <div className="characters__property button button--red" onClick={() => actions.deleteCharacterAction(i)}>Delete</div>
         </div>
       ))}
     </div>
